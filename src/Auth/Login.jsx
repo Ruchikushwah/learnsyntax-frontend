1c3be208673dcc9176ceb1d02fe4da5eb342f758
ruchikushwah <ruchi741@bgpgmail.com>
2024-12-11 14:14:34 +0530

    register login completed

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-export default Login
+    const handleLogin = async () => {
+        const data = {email:email,password:password};
+        let resp = await fetch("http://127.0.0.1:8000/api/auth/login",{
+            method:"POST",
+            body:JSON.stringify(data),
+            headers:{
+                "Content-Type" :"application/json",
+            },
+        });
+        resp = await resp.json();
+        console.log(resp);
+        
+        localStorage.setItem("token", resp.access_token);
+        if (resp.user) {
+          navigate("/");
+        }
+    };
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 px-4 py-8">
+      <div className="w-full max-w-md rounded-xl bg-white p-8 shadow-lg gap-5 flex flex-col">
+        <div className="flex flex-col ">
+         
+          <p className="mb-4 text-center text-lg font-semibold text-gray-600">
+            Please log in to continue
+          </p>
+        </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email Address
+          </label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            id="email"
+            className="w-full mt-1 rounded-lg border-gray-300 px-4 py-2 text-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            id="password"
+            className="w-full mt-1 rounded-lg border-gray-300 px-4 py-2 text-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+            placeholder="Enter your password"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          onClick={() => handleLogin()}
+          className="w-full rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 px-4 py-2 text-lg font-semibold text-white shadow-lg transition-transform duration-300 ease-in-out hover:scale-105 hover:from-blue-600 hover:to-purple-600"
+        >
+          Log In
+        </button>
+        <p className="mt-6 text-center text-sm text-gray-600">
+          Don’t have an account?{" "}
+          <a
+            href="register"
+            className="font-medium text-purple-600 hover:underline"
+          >
+            Sign up
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleRegister = async () => {
+    const data = { name, email, password };
 
-export default Register
+    try {
+      let resp = await fetch("http://127.0.0.1:8000/api/auth/register", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      resp = await resp.json();
+      alert(resp.message);
+
+      if (resp.user) {
+        
+        navigate("/login"); 
+      }
+    } catch (error) {
+      console.error("Error during registration:", error);
+      alert("Registration failed. Please try again.");
+    }
+  };
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100 px-4 py-8">
+      <div className="w-full max-w-md rounded-xl bg-white p-8 shadow-lg gap-5 flex flex-col">
+        <h2 className="text-2xl font-bold text-center text-gray-700">
+          Register
+        </h2>
+
+        <div>
+          <label className="block text-sm font-medium text-gray-600">
+            Name
+          </label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="name"
+            placeholder="Enter Your Name"
+            className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            required
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-600">
+            Email
+          </label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="Enter Your Email"
+            className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            required
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-600">
+            Password
+          </label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Enter Password"
+            className="w-full px-4 py-2 mt-1 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            required
+          />
+        </div>
+        <button
+          onClick={() => handleRegister()}
+          type="submit"
+          className=" px-4 py-2 font-semibold text-white bg-blue-500 rounded-lg hover:bg-blue-600"
+        >
+          Sign up
+        </button>
+        <p className="text-sm text-center text-gray-600">
+          Don’t have an account?{" "}
+          <a href="login" className="text-blue-500 hover:underline">
+            Sign in
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
