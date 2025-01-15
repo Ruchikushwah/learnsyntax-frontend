b20e30c6f19842136d8dbef83ccca37d87a305b2
akankshaverm <priyaakanksha0507@gmail.com>
2025-01-15 16:05:17 +0530

    fixed the design  when we course and post image will be inserted

--- src/Admin/InsertCourse.jsx
+++ src/Admin/InsertCourse.jsx
@@ -100,44 +100,45 @@ const InsertCourse = () => {
           >
             Course Description
           </label>
-          
-            <ReactQuill
-              theme="snow"
-              value={description}
-              onChange={setDescription}
-              className="bg-white  w-full h-36"
-              
-            />
+
+          <ReactQuill
+            theme="snow"
+            value={description}
+            onChange={setDescription}
+            className="bg-white  w-full h-36"
+          />
           {errors.description && (
             <p className="text-red-500 text-sm">{errors.description}</p>
           )}
         </div>
 
-        <div class="flex items-center justify-center w-full mb-4 py-2">
+        <div className="flex items-center justify-center w-full mb-4 py-2">
           <label
-            for="dropzone-file"
-            className="flex flex-col items-center justify-center w-full h-36 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 "
+            htmlFor="dropzone-file"
+            className="flex flex-col items-center justify-center w-full h-36 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500"
           >
-            <div class="flex flex-col items-center justify-center pt-5 pb-6">
-              <svg
-                className="w-8 h-8  text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 16"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                />
-              </svg>
-              <p class=" text-sm text-gray-500 dark:text-gray-400">
-                <span className="font-semibold">Select Course Image</span>
-              </p>
-            </div>
+            {!image && ( // Only showing svg if no any image is selected
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg
+                  className="w-8 h-8 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                  />
+                </svg>
+                <p className="text-sm text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Select Course Image</span>
+                </p>
+              </div>
+            )}
             <input
               id="dropzone-file"
               type="file"
@@ -148,7 +149,7 @@ const InsertCourse = () => {
               <img
                 src={URL.createObjectURL(image)} // For Live Image
                 alt="Preview"
-                className=" w-auto  h-modal  flex justify-center items-center top-16 mb-2"
+                className="w-auto h-modal flex justify-center items-center top-16 mb-2"
               />
             )}
             {errors.imagePath && (
